5adb4d82672f6f16e4d792b6365bb4b5b2d57664
Renan Vaz <renan.c.vaz@gmail.com>
2014-02-23 02:58:33 -0300

    remove _this

--- src/js/Class.js
+++ src/js/Class.js
@@ -5,20 +5,19 @@ Class.prototype.init = function(){ };
 
 Class.extend = function (p) {
     var i,
-        _this = this,
         _class = new Function('return function(){ var i; this.super = {}; for (i in this._super) { if (i !== "_super") { this.super[i] = this._super[i].bind(this); } } this.init.apply(this, arguments); };')();
 
     _class.extend = this.extend;
 
-    for (i in _this.prototype) {
-        _class.prototype[i] = _this.prototype[i];
+    for (i in this.prototype) {
+        _class.prototype[i] = this.prototype[i];
     }
 
     for (i in p) {
         _class.prototype[i] = p[i];
     }
 
-    _class.prototype._super = _this.prototype;
+    _class.prototype._super = this.prototype;
 
     return _class;
 }
